f44fd43246e1d9ac46a74e855573af3084947f08
Romain <romain.reydel@gmail.com>
2024-03-17 15:55:07 +0100

    test : ajout parser args invalid

---
+++ TP3/src/ArgumentParser.cs
@@ -0,0 +1,14 @@
+﻿namespace CredImmo.App
+{
+    public sealed class ArgumentParser
+    {
+        private string[] args;
+
+        public ArgumentParser(string[] args)
+        {
+            if(args.Length < 3)
+                throw new ArgumentException("Le nombre d'arguments doit être égal à 3");
+            this.args = args;
+        }
+    }
+}
---
+++ TP3/test/CreditImmoUserInteractions.cs
@@ -0,0 +1,17 @@
+﻿using CredImmo.App;
+
+namespace CredImmo.Test
+{
+    public class CreditImmoUserInteractions
+    {
+        [Fact]
+        public void ParseInput_InvalidCount()
+        {
+            string[] args = ["50000", "9"];
+
+            ArgumentException exception = Assert.Throws<ArgumentException>( () =>  new ArgumentParser(args));
+
+            Assert.Equal("Le nombre d'arguments doit être égal à 3", exception.Message);
+        }
+    }
+}
