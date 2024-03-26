a69d9d03920cbbf12ecea5f007d82deaae90a66c
Romain <romain.reydel@gmail.com>
2024-03-26 20:10:14 +0100

    test : execution du programme

--- TP3/src/Program.cs
+++ TP3/src/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace CredImmo.App
+﻿using CredImmo.App.Models;
+
+namespace CredImmo.App
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Execute(args));
+        }
+
+        public static string Execute(string[] args)
+        {
+            try
+            {
+                ArgumentParser parser = new ArgumentParser(args);
+                CreditImmo creditImmo = new CreditImmo(parser.Montant, parser.Duree, parser.Taux);
+
+                if (Validator.IsValid(creditImmo, out string outputMessage))
+                {
+                    CreditImmoResultat resultat = CalculCreditImmo.Calcul(creditImmo);
+                    return CSVWriter.Write(resultat);
+                }
+                else
+                {
+                    return outputMessage;
+                }
+            }
+            catch (Exception exc)
+            {
+                return exc.Message;
+            }
         }
     }
 }
--- TP3/test/CreditImmoProgram.cs
+++ TP3/test/CreditImmoProgram.cs
@@ -10,7 +10,7 @@ namespace CredImmo.Test
         {
             string[] args = ["100000", "240"];            
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Equal("Le nombre d'arguments doit être égal à 3", userMessage);
         }
@@ -20,7 +20,7 @@ namespace CredImmo.Test
         {
             string[] args = ["0", "108", "1.2"];
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Equal("Le montant de l'emprunt doit être >= 50 000€", userMessage);
         }
@@ -30,7 +30,7 @@ namespace CredImmo.Test
         {
             string[] args = ["50000", "107", "1.2"];
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Equal("La durée de l'emprunt doit être >= 9 ans", userMessage);
         }
@@ -40,7 +40,7 @@ namespace CredImmo.Test
         {
             string[] args = ["50000", "301", "1.2"];
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Equal("La durée de l'emprunt doit être <= 25 ans", userMessage);
         }
@@ -50,7 +50,7 @@ namespace CredImmo.Test
         {
             string[] args = ["50000", "108", "-0.1"];
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Equal("Le taux de l'emprunt doit être >= 0", userMessage);
         }
@@ -60,12 +60,10 @@ namespace CredImmo.Test
         {
             string[] args = ["50000", "108", "1.2"];
 
-            string userMessage = Main.Execute(args);
+            string userMessage = Program.Execute(args);
 
             Assert.Contains("creditImmo_", userMessage);
-            Assert.Contains(".csv", userMessage);
-
-            File.Delete(userMessage);
+            Assert.Contains(".csv", userMessage);                        
         }
     }
 }
