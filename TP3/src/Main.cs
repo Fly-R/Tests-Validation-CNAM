f07ffe2c2c9e81b2d31fc4c9445142f9ca1161ce
Romain <romain.reydel@gmail.com>
2024-03-26 19:53:18 +0100

    test : execution du programme

---
+++ TP3/src/Main.cs
@@ -0,0 +1,14 @@
+﻿using CredImmo.App.Models;
+
+namespace CredImmo.App
+{
+    public static class Main
+    {
+
+        public static string Execute(string[] args)
+        {
+            return string.Empty;          
+        }
+
+    }
+}
---
+++ TP3/test/CreditImmoProgram.cs
@@ -0,0 +1,71 @@
+﻿using CredImmo.App;
+
+namespace CredImmo.Test
+{
+    public class CreditImmoProgram
+    {
+
+        [Fact]
+        public void CreditImmoProgram_NombreArgumentIncorrect()
+        {
+            string[] args = ["100000", "240"];            
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Equal("Le nombre d'arguments doit être égal à 3", userMessage);
+        }
+
+        [Fact]
+        public void CreditImmoProgram_MontantEmpruntIncorrect()
+        {
+            string[] args = ["0", "108", "1.2"];
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Equal("Le montant de l'emprunt doit être >= 50 000€", userMessage);
+        }
+
+        [Fact]
+        public void CreditImmoProgram_DureeEmpruntInferieure()
+        {
+            string[] args = ["50000", "107", "1.2"];
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Equal("La durée de l'emprunt doit être >= 9 ans", userMessage);
+        }
+
+        [Fact]
+        public void CreditImmoProgram_DureeEmpruntSuperieure()
+        {
+            string[] args = ["50000", "301", "1.2"];
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Equal("La durée de l'emprunt doit être <= 25 ans", userMessage);
+        }
+
+        [Fact]
+        public void CreditImmoProgram_TauxIncorrect()
+        {
+            string[] args = ["50000", "108", "-0.1"];
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Equal("Le taux de l'emprunt doit être >= 0", userMessage);
+        }
+
+        [Fact]
+        public void CreditImmoProgram_ValeursCorrectes()
+        {
+            string[] args = ["50000", "108", "1.2"];
+
+            string userMessage = Main.Execute(args);
+
+            Assert.Contains("creditImmo_", userMessage);
+            Assert.Contains(".csv", userMessage);
+
+            File.Delete(userMessage);
+        }
+    }
+}
